323b54b4618fad00397af28c51b6fa4df095b4e6
MakFil <makfil1177@gmail.com>
2022-01-30 11:15:39 +0200

    refactor: remove fastify src folder

--- src/common/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { statusCodes } from './statusCodes';
-export { loggingLevel } from './loggingLevel';
-export { logger } from './logger';
-export { handleLogging } from './logger';
-export { hashCreate, hashCheck } from './hashHandler';
--- src/resources/index.ts
+++
@@ -1,4 +0,0 @@
-export { UserEntity } from './users/user.model';
-export { BoardEntity } from './boards/board.model';
-export { ColumnEntity } from './boards/column.model';
-export { TaskEntity } from './tasks/task.model';
